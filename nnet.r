71fc70d1698e323220c639897273768c03cf3492
NewPalmHostel <newpalmhostel@users.noreply.github.com>
2016-03-10 17:10:48 +0900

    add .nnet2w

--- nnet.r
+++ nnet.r
@@ -3,3 +3,22 @@ library( nnet )
 ## nnet for regression
 nn <- nnet( x=xs, y=ys, size=num.hid, linout=TRUE, rang=4/sqrt( 6/(dim.in + dim.out) ), trace=FALSE, MaxNWts=2000, maxit=1000 )
 predict( nn, newdata=x.test, type="raw" )
+
+## extract weights as matrices
+.nnet2w <- function( nn ){
+    dim.in  <- nn$n[1]
+    num.hid <- nn$n[2]
+    dim.out <- nn$n[3]
+    vector  <- nn$wts ## [i0->h1, i1->h1, ..., i.dim.in->h.num.hid;
+                      ##  h0->o1, h1->o1, ..., h.num.hid->o.dim.out]
+    idx.mid <- (dim.in + 1) * num.hid
+
+    ## [ num.hid (dim.in+1) ]
+    ## the bias (+b) is assigned for the first column
+    in2hid  <- matrix( vector[1:idx.mid], ncol=dim.in+1, byrow=TRUE )
+    ## [ dim.out (num.hid+1) ]
+    ## the bias (+b) is assigned for the first column
+    hid2out <- matrix( vector[-(1:idx.mid)], ncol=num.hid+1, byrow=TRUE )
+
+    return( list( in2hid=in2hid, hid2out=hid2out ) )
+}
